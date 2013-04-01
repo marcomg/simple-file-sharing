b9ea3368c9db5f74584d7d37fe573b49a90f3642
marcomg <mmarcogguerrini@gmail.com>
2013-04-01 12:33:22 +0200

    Rimosso bug per il controllo aggiornamento

--- changelog.md
+++ changelog.md
@@ -2,6 +2,7 @@
 # 1.0 branch #
 ## 1.0.3-beta ##
 * Corrected 404 bug
+* Removed update bug
 * The pages always have the title set
 
 ## 1.0.2-beta ##
--- script/update.php
+++ script/update.php
@@ -27,7 +27,7 @@ $smarty->assign('title', $string['title_update']);
 
 // Prelevo la nostra versione
 $_ = file(ROOT.'/version.txt');
-$our_version = $_[0];
+$our_version = str_replace(PHP_EOL, null, $_[0]);
 
 // Prelevo la versione attuale, se ce un errore lo stampo
 $_ = file('https://bitbucket.org/marcomg/simple-file-sharing/raw/master/version.txt');
@@ -37,13 +37,12 @@ if(!$_){
     $version = 0;
 }
 else{
-    $version = $_[0];
+    $version = str_replace(PHP_EOL, null, $_[0]);
 }
 
 // Controllo se è da aggiornare solo se la pagina è stata scaricata
 if($_ !== false){
     $_ = version_compare($our_version, $version);
-    echo $_;
     // Se sono disponibili aggiornamenti
     if($_ == -1){
         $smarty->assign('available', true);
